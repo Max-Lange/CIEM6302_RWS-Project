0e05cabc111923b00abb0a4711a3cce7318c67e1
Wailabd <wail.abdellaoui@student.tudelft.nl>
2023-11-02 16:48:26 +0100

    p

--- Pages/6_Evaluation.py
+++ Pages/6_Evaluation.py
@@ -1,25 +1,17 @@
 import streamlit as st
 
-
-
-
-
 # Result section
 st.header("Evaluation")
 st.write("In this paragraph we will have a look at the final result with respect to the main goal of the research")
 
 st.subheader("Result:")
-st.write("Automatic inspector placement based on accident probabilities")
+st.write("The figure below demonstrates that the vast majority of the country is accessible to inspectors with a travel time of up to 15 minutes.")
+st.write("We have succeeded in achieving automatic inspector placement based on accident probabilities.")
 
 # Load the cover image with caption
 cover_image = st.image("cover.png", use_column_width=True, caption="100 Inspectors coverage map")
 
 
-# Future Steps section
-st.subheader("Future Steps:")
-st.markdown("• Qualitative Testing", unsafe_allow_html=True)
-st.markdown("• Remove network flaws", unsafe_allow_html=True)
-st.markdown("• Simulate to predict Performance", unsafe_allow_html=True)
 
 
 st.subheader("Expected Final Outcome")
@@ -27,17 +19,17 @@ st.write("The following requirements have been tried to satisfy for the optimiza
 
 
 method_checkbox = st.checkbox("Method to identify the locations of road inspectors to reach the hotspots < 18 mins", value=True)
-evaluate_checkbox = st.checkbox("Evaluate the method for different scenarios")
+evaluate_checkbox = st.checkbox("Evaluate the method for different scenarios", value=True)
 explanations_checkbox = st.checkbox("Explanations of the method(s) with respective results")
 incident_types_checkbox = st.checkbox("Nice to have: Locations based on incident types")
 incident_characteristics_checkbox = st.checkbox("Nice to have: Location based on incident characteristics")
 
 # You can use the values of these checkboxes in your app logic
 if method_checkbox:
-    st.write("A method has been succesfully established to identify the locations of road inspectors to reach the hotspots < 18 mins!")
+    st.write("- A method has been succesfully established to identify the locations of road inspectors to reach the hotspots < 18 mins!")
 
 if evaluate_checkbox:
-    st.write("Evaluate the method for different scenarios")
+    st.write("- The developed algorithm is successfully capable of evaluating the method for different scenarios, including the number of road inspectors, considering various data, time frames, and types of incidents. Based on these scenarios, an inspector distribution can be determined.")
 
 if explanations_checkbox:
     st.write("Explanations of the method(s) with respective results")
@@ -49,3 +41,10 @@ if incident_characteristics_checkbox:
     st.write("Nice to have: Location based on incident characteristics")
 
 
+# Future Steps section
+st.subheader("Future Steps:")
+st.write("We have successfully identified an inspector distribution. The next steps for Rijkswaterstaat to conduct are:")
+
+st.markdown("• Qualitative Testing")
+st.markdown("• Remove network flaws")
+st.markdown("• Simulate to predict Performance")
